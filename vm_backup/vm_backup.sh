3d6e5f7997347bb3909baa5ce8c9e112bfd9097e
Fabian Schlager <fabian.schlager@skyr.at>
2017-09-03 01:45:23 +0200

    Add vm_create and vm_backup

---
+++ vm_backup/vm_backup.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# Settings
+BACKUP_DISK_SIZE=100G
+AWS_USER=aws
+AWS_VAULT_NAME=Reefer
+MAIL_FROM=backup@nukularstrom.de
+MAIL_TO=tyrant@nukularstrom.de
+SMTP_SERVER=192.168.100.106:587
+MAIL_ON_SUCCESS=1
+BACKUP_VM=( gitlab database )
+
+send_mail()
+{
+  local subject=$1
+  local message=$2
+
+  echo "$message" | heirloom-mailx -r "$MAIL_FROM" -s "[Backup] $subject" -S smtp="$SMTP_SERVER" -S smtp-use-starttls -S ssl-verify=ignore $MAIL_TO
+}
+
+run_cmd_log()
+{
+  msg=$("$@")
+  local status=$?
+  echo "$msg" >> /tmp/backup.log
+  if [ $status -ne 0 ]; then
+      echo "Command '$@' failed with $status" >> /tmp/backup.log
+
+      send_mail "Error" "$(cat /tmp/backup.log)"
+      exit 1
+  fi
+
+  return $status
+}
+
+run_cmd()
+{
+  msg=$("$@")
+  local status=$?
+  if [ $status -ne 0 ]; then
+      echo "$msg" >> /tmp/backup.log
+      echo "Command '$@' failed with $status" >> /tmp/backup.log
+
+      send_mail "Error" "$(cat /tmp/backup.log)"
+      exit 1
+  fi
+
+  return $status
+}
+
+mount_lvm()
+{
+  local lv_name=$1
+  local lv_path="/dev/vg0/lv_$lv_name"
+  local lv_bck_name=lv_${lv_name}_bck
+  local lv_bck_path=${lv_path}_bck
+
+  # Create a snapshot and mount it
+  run_cmd lvcreate -s -L$BACKUP_DISK_SIZE -n $lv_bck_name $lv_path
+  run_cmd kpartx -a -s $lv_bck_path
+
+  sleep 1
+  local mount_dir=$(mktemp -d) 
+  run_cmd mount /dev/${lv_name}-vg/root $mount_dir
+
+  echo $mount_dir
+  return 0
+}
+
+unmount_lvm()
+{
+  local lv_name=$1
+  local lv_path="/dev/vg0/lv_$lv_name"
+  local lv_bck_name=lv_${lv_name}_bck
+  local lv_bck_path=${lv_path}_bck
+  local mount_dir=$2
+
+  run_cmd umount $mount_dir
+  run_cmd rm -r $mount_dir
+
+  # Clean Up
+  sleep 1
+  run_cmd dmsetup remove ${lv_name}--vg-root
+  run_cmd dmsetup remove ${lv_name}--vg-swap_1
+
+  sleep 1
+  run_cmd kpartx -d $lv_bck_path
+  run_cmd dmsetup remove vg0-${lv_bck_name}
+  run_cmd dmsetup remove vg0-${lv_bck_name}-cow
+
+  run_cmd lvremove -y $lv_bck_path
+}
+
+backup()
+{
+  local lv_name=$1
+  local mount_dir=$2
+
+  local timestamp=$(date +"%Y%m%d%H%M")
+  local archive_name="${lv_name}-${timestamp}.tar.gz"
+
+  run_cmd chroot $mount_dir ./usr/share/backup/backup.sh "/$archive_name"
+  echo $archive_name >> /tmp/backup.log
+  run_cmd_log su -c "aws glacier upload-archive --account-id - --vault-name '$AWS_VAULT_NAME' --archive-description '$archive_name' --body '$mount_dir/$archive_name'" $AWS_USER
+}
+
+echo "Backup starting ..." > /tmp/backup.log
+
+for i in "${BACKUP_VM[@]}"
+do
+  echo "############# $i" >> /tmp/backup.log
+  mount_dir=$(mount_lvm $i)
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+
+  backup $i $mount_dir
+  unmount_lvm $i $mount_dir
+done
+
+if [ $MAIL_ON_SUCCESS -eq 1 ]; then
+  send_mail "Success" "$(cat /tmp/backup.log)"
+fi
---
+++ vm_create/vm_create.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import hashlib
+import subprocess
+import xml.etree.ElementTree as ET
+
+import yaml
+
+
+def generate_mac_address(name):
+  checksum = hashlib.sha256(name.encode('utf-8')).hexdigest()
+  return '02:%s:%s:%s:%s:%s' % (checksum[0:2], checksum[2:4], checksum[4:6], checksum[6:8], checksum[8:10])
+
+def create_logical_volume(name, size):
+  print("Creating logical volume '%s' ..." % (name))
+  return subprocess.run(['lvcreate', '-y', '-L', size, '-n', name, 'vg0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def delete_logical_volume(name):
+  print("Deleting logical volume '%s' ..." % (name))
+  return subprocess.run(['lvremove', '-y', '/dev/vg0/%s' % name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def remove_from_dhcp(name, mac, ip):
+  print("Removing '%s' from DHCP ..." % (name))
+
+  xml = "<host name='%s' mac='%s' ip='%s'/>" % (name, mac, ip)
+
+  return subprocess.run(['virsh',
+    'net-update', 'default', 'delete', 'ip-dhcp-host',
+    xml,
+    '--live', '--config'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def destroy_virtual_machine(name):
+  print("Shutting down '%s' ..." % (name))
+
+  return subprocess.run(['virsh',
+    'destroy', name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def delete_virtual_machine(name):
+  print("Undefining virtual machine '%s' ..." % (name))
+
+  return subprocess.run(['virsh',
+    'undefine', name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def create_virtual_machine(name, cores, memory, lv_name, mac):
+  print("Creating virtual machine '%s' ..." % (name))
+
+  return subprocess.run(['virt-install',
+    '--name', name,
+    '--vcpus', str(cores),
+    '--memory', str(memory),
+    '--disk', 'path=/dev/vg0/%s' % lv_name,
+    '--cdrom=/var/lib/libvirt/images/debian-9.1-amd64-preseed.iso',
+    '--os-type=linux',
+    '--os-variant=debianwheezy',
+    '--network', 'default,mac=%s' % (mac),
+    '--graphics=spice,listen=0.0.0.0',
+    '--debug',
+    '--console', 'pty,target_type=serial',
+  ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def add_to_dhcp(name, mac, ip):
+  print("Adding '%s' to DHCP ..." % (name))
+
+  xml = "<host name='%s' mac='%s' ip='%s'/>" % (name, mac, ip)
+
+  subprocess.run(['virsh',
+    'net-update', 'default', 'delete', 'ip-dhcp-host',
+    xml,
+    '--live', '--config'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+  return subprocess.run(['virsh',
+    'net-update', 'default', 'add', 'ip-dhcp-host',
+    xml,
+    '--live', '--config'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def sanity_check(config):
+  # Check if a logical volume with this name already exists
+  if os.path.exists('/dev/vg0/%s' % config['lv_name']):
+      print("Logical volume '%s' already exists!" % (config['lv_name']))
+      return False
+
+  # Check if a machine with this name already exists
+  result = verify_run(subprocess.run(['virsh', 'list', '--all', '--name'], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+  if config['name'] in result.stdout.decode('utf-8').split('\n'):
+    print("Machine with this name already exists!")
+    return False
+
+  # Check if this IP is already in use by a different machine
+  result = verify_run(subprocess.run(['virsh', 'net-dumpxml', 'default'], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+  xml = ET.fromstring(result.stdout)
+  for host in xml.findall('./ip/dhcp/host'):
+    if host.attrib['ip'] == config['ip']:
+      if host.attrib['name'] != config['name'] or host.attrib['mac'] != config['mac']:
+        print("IP address '%s' already in use by '%s' (MAC: %s)!" % (host.attrib['ip'], host.attrib['name'], host.attrib['mac']))
+        return False
+
+  return True
+
+
+def verify_run(process):
+  if process.returncode != 0:
+    print("STDOUT:")
+    print(process.stdout.decode('utf-8'))
+    print("STDERR:")
+    print(process.stderr.decode('utf-8'))
+
+    sys.exit(-1)
+
+  return process
+
+
+def main(mode, path):
+  config = {}
+
+  with open(path, 'r') as stream:
+    try:
+      config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+      print(exc)
+      sys.exit(-1)
+
+  vm_name = next(iter(config))
+
+  config = config[vm_name]
+  config['name'] = vm_name
+  config['mac'] = generate_mac_address(vm_name)
+  config['ip'] = '192.168.100.%d' % (config['ip'])
+  config['lv_name'] = 'lv_%s' % (vm_name)
+
+
+  if mode == 'create':
+    print("Creating vm '%s' with the following configuration:" % (vm_name))
+    print(config)
+
+    if not sanity_check(config):
+      sys.exit(-1)
+
+    verify_run(create_logical_volume(config['lv_name'], config['disk']))
+    verify_run(add_to_dhcp(vm_name, config['mac'], config['ip']))
+    verify_run(create_virtual_machine(vm_name, config['cores'], config['memory'], config['lv_name'], config['mac']))
+  elif mode == 'delete':
+    print("Deleting vm '%s' with the following configuration:" % (vm_name))
+    print(config)
+
+    verify_run(destroy_virtual_machine(config['name']))
+    verify_run(remove_from_dhcp(vm_name, config['mac'], config['ip']))
+    verify_run(delete_virtual_machine(vm_name))
+    verify_run(delete_logical_volume(config['lv_name']))
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage: vm_create.py create|delete <path/to/server.yaml>")
+    sys.exit(-1)
+
+  main(sys.argv[1], sys.argv[2])
